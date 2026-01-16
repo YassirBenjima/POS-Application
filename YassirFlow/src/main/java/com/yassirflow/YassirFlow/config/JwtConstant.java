522e682df5f5f9ea51a2b5ac57cdad6a0e91540f
Yassir Benjima <yassirbenjima18@GMAIL.com>
2026-01-16 18:30:08 +0100

    feat: Implement JWT-based authentication and authorization using Spring Security.

---
+++ YassirFlow/src/main/java/com/yassirflow/YassirFlow/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.yassirflow.YassirFlow.config;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "swfadsfasdfa.fasdfasdfasd.fweqwefqwe.juyjtjryrj";
+    public static final String JWT_HEADER = "Authorization";
+}
