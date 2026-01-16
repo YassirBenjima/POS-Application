b88d0d8257c57b107f8b49492bbb774851ca1a0c
Yassir Benjima <yassirbenjima18@GMAIL.com>
2026-01-16 17:46:45 +0100

    first commit

---
+++ YassirFlow/src/main/java/com/yassirflow/YassirFlow/YassirFlowApplication.java
@@ -0,0 +1,13 @@
+package com.yassirflow.YassirFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YassirFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YassirFlowApplication.class, args);
+	}
+
+}
---
+++ YassirFlow/src/test/java/com/yassirflow/YassirFlow/YassirFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yassirflow.YassirFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YassirFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
